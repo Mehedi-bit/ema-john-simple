aa655de6db34fccdb65a7a062cd7ad1b771f40a5
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2021-04-29 12:28:26 +0600

    ok bro

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
+
+    const [cart, setCart] = useState([]);
+    useEffect(()=>{
+        //cart
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const cardProducts = productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            console.log(product);
+            return product;
+        });
+        setCart(cardProducts);
+    }, [])
     return (
         <div>
-            <h1>This is Review</h1>
+            <h1>Cart items: {cart.length}</h1>
+            {
+                cart.map(pd => <ReviewItem product={pd}
+                                            key={pd.key}></ReviewItem>)
+            }
         </div>
     );
 };
